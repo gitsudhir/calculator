4299c99ff1e3c1ba6b952f1280a8815a8ffb822e
gitsudhir <31241120+gitsudhir@users.noreply.github.com>
2019-02-08 23:39:59 +0530

    Updates

---
+++ precache-manifest.d006d67d5a2452a0acce252c48732ced.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demoReact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0b5cec823fa0fbf2636e",
+    "url": "/demoReact/static/js/runtime~main.0b5cec82.js"
+  },
+  {
+    "revision": "f095161ce41d6a0406d9",
+    "url": "/demoReact/static/js/main.f095161c.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/demoReact/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "f095161ce41d6a0406d9",
+    "url": "/demoReact/static/css/main.93627886.chunk.css"
+  },
+  {
+    "revision": "fb8b2d82aef82b374e7355feb4130e69",
+    "url": "/demoReact/index.html"
+  }
+];
