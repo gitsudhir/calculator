219bc673a87fac7183602f4dd4671f2104a64a64
gitsudhir <31241120+gitsudhir@users.noreply.github.com>
2019-02-14 12:14:13 +0530

    initial commit

---
+++ src/Button.jsx
@@ -0,0 +1,10 @@
+import React  from 'react' ;
+import './App.css'
+//import {output} from './App'
+export  const Button = ({name}) =>{
+   // const [age ,setAge]=useS 
+   return (
+ <button className="btn" onClick={()=>output+=name}>{name}</button>
+    )  
+} ;
+ 
---
+++ src/calculator.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./App.css";
+
+export const Calculator = () => {
+const [input,setInput]=useState('')
+const [ans,setAns]=useState(0) 
+
+function takeInput(e){
+    //console.log(e.target);
+    if(e.target.innerHTML == '=' || e.target.innerHTML == 'back'){
+        try{
+       let evalValue = eval(input) 
+        setAns( evalValue )  ;
+        }catch{
+            console.log('wrong ')
+        }
+    }else{
+        setInput(input+e.target.innerHTML)
+    }
+    
+    if(e.target.innerHTML == 'back'){
+        setInput(input.slice(0,-1))
+        console.log('from back')
+    }
+
+
+    //console.log(e.target.innerHTML);
+}
+
+
+  return (
+    <div className>
+      <div className="flex"> <div className="input">{input}</div>   </div>
+      <div className="flex"> <div className="ans">{ans}</div> </div>
+      <div className="flex"><button onClick={takeInput} className="btn">/</button><button onClick={takeInput} className="btn">*</button><button onClick={takeInput} className="btn-big">back</button></div>
+      <div className="flex"><button onClick={takeInput} className="btn">7</button><button onClick={takeInput} className="btn">8</button><button onClick={takeInput} className="btn">9</button><button onClick={takeInput} className="btn">-</button></div>
+      <div className="flex"><button onClick={takeInput} className="btn">4</button><button onClick={takeInput} className="btn">5</button><button onClick={takeInput} className="btn">6</button><button onClick={takeInput} className="btn">+</button></div>
+      <div className="flex"><button onClick={takeInput} className="btn">1</button><button onClick={takeInput} className="btn">2</button><button onClick={takeInput} className="btn">3</button><button onClick={takeInput} className="btn">.</button></div>
+      <div className="flex"><button onClick={takeInput} className="btn-big">0</button><button onClick={takeInput} className="btn-big">=</button></div>
+     
+    </div>
+  );
+};
